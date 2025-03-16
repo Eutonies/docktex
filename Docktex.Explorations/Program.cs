07f0ad9fd66f21478ddb8d715e6533d78962d7e1
Sune Rønne <SURN@nykredit.dk>
2025-03-16 19:43:39 +0100

    Enough bullshit!

---
+++ Docktex.Explorations/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configBuilder = new ConfigurationBuilder();
+configBuilder
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile("appsettings.local.json", optional: true)
+    .AddEnvironmentVariables();
+
+
